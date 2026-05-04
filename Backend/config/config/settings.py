9879ab6846e5542430e8e11ec10541902a4856b6
sanskar ranjan <122105768+sanss-kar@users.noreply.github.com>
2026-05-04 13:57:33 +0530

    Update CORS_ALLOWED_ORIGINS for deployment

--- Backend/config/config/settings.py
+++ Backend/config/config/settings.py
@@ -85,7 +85,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://sanss-kar-github-io.vercel.app/",
+    "https://task-management.vercel.app",
 ]
 
 ROOT_URLCONF = 'config.urls'
