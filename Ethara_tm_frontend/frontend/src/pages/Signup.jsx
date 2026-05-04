75cfebc46c32372ce6e008b575466469dce4de2e
sanskar ranjan <122105768+sanss-kar@users.noreply.github.com>
2026-05-04 14:05:22 +0530

    Fix import statement for API module

--- Ethara_tm_frontend/frontend/src/pages/Signup.jsx
+++ Ethara_tm_frontend/frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import API from "../api/axios";
+import api from "../api/axios";
 
 function Signup() {
   const navigate = useNavigate();
